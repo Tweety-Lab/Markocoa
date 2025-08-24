9401ade38cf5a3e1cf36497054824bb971232d15
Tweety-lab <108560864+Tweety-lab@users.noreply.github.com>
2025-08-24 19:00:04 +1000

    Add 'Themes/' Folder.

---
+++ Markocoa/Themes/ThemeSettings.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Markocoa.Themes;
+
+/// <summary>
+/// Object representing the theme settings.
+/// </summary>
+/// <remarks>This class gets serialized to and from a YAML file.</remarks>
+internal class ThemeSettings
+{
+    /// <summary>
+    /// The name of the theme.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    /// Path to the page template file.
+    /// </summary>
+    public string? PageTemplate { get; set; }
+}
---
+++ Markocoa/Utilities/TemplateEngine.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Markocoa.Utilities;
+
+/// <summary>
+/// Utility class for handling template rendering.
+/// </summary>
+internal static class TemplateEngine
+{
+}
